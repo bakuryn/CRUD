88fc055b9138b9765addf1ba1679c1a21b5a1447
Francisco Ribeiro <104844729+bakuryn@users.noreply.github.com>
2022-10-09 21:28:17 -0300

    banco atualizado

--- locadora/db/conn.js
+++ locadora/db/conn.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('locadora', 'root', '', {
+const sequelize = new Sequelize('locadora1', 'root', '', {
     host:'localhost',
     dialect: 'mysql',
 })
